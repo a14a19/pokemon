ee894fb2cfa9b8114a6fceb944de2b9d41ede1f0
a14a19 <anas31197@gmail.com>
2022-09-25 16:59:00 +0530

    initial commit

---
+++ starwar/script.js
@@ -0,0 +1,51 @@
+const nameSw = document.getElementById("name");
+const heightSw = document.getElementById("height");
+const massSw = document.getElementById("mass");
+const homeworld = document.getElementById("homeworld");
+const hairColor = document.getElementById("hair-color");
+const button = document.querySelector(".get-random-charac");
+const skinColor = document.getElementById("skin-color");
+const birthYear = document.getElementById("birth-year"); 
+const genderSw = document.getElementById("gender"); 
+
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    nameSw.innerText = `Loading...`;
+    heightSw.innerText = `Loading...`;
+    massSw.innerText = `Loading...`;
+    hairColor.innerText = `Loading...`;
+    EyeColor.innerText = `Loading...`;
+    skinColor.innerText = `Loading...`;
+    birthYear.innerText = `Loading...`;
+    genderSw.innerText = `Loading...`;
+    
+    const numRandom = parseInt(Math.ceil(Math.random()*83));
+
+    const urlSw = "https://swapi.dev/api/people/";
+
+    fetch(`${urlSw}${numRandom}`)
+    .then(Response => Response.json())
+    .then(character => {
+        // console.log(character.name)
+
+        const {name, height, mass, hair_color, eye_color, skin_color, birth_year, gender} = character
+
+        if(name, height, mass, hair_color, eye_color, skin_color, birth_year, gender == ""){
+            throw new Error("Undefined")
+        }
+
+        nameSw.innerText = `Name: ${name}`;
+        heightSw.innerText = `Height: ${height}`;
+        massSw.innerText = `Mass: ${mass}`;
+        hairColor.innerText = `Hair color: ${hair_color}`;
+        EyeColor.innerText = `Eye Color: ${eye_color}`;
+        skinColor.innerText = `Skin color: ${skin_color}`;
+        birthYear.innerText = `Birth year: ${birth_year}`;
+        genderSw.innerText = `Gender: ${gender}`;
+    })
+    
+})
+
+
+    
